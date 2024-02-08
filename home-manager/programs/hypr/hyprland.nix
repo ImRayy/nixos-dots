673f6f7991f03f9dfce1426aeeb52003b8280962
RAY <souradeep_roy@pm.me>
2024-02-08 23:59:07 +0530

    Hyprland: updated workspace animation

--- home-manager/programs/hypr/hyprland.nix
+++ home-manager/programs/hypr/hyprland.nix
@@ -33,14 +33,12 @@
             animations = {
                 enabled = "yes";
                 bezier = "quart, 0.25, 1, 0.5, 1";
-
-
                 animation = [
                     "windows, 1, 6, quart, slide"
                     "border, 1, 6, quart"
                     "borderangle, 1, 6, quart"
                     "fade, 1, 6, quart"
-                    "workspaces, 1, 6, default, slidefade 20%"
+                    "workspaces, 1, 6, default"
                 ];
             };
             dwindle = {
