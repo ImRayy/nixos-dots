07bf1b6ae260b60fe84ec52b670309d7fd4a1b20
RAY <souradeep_roy@pm.me>
2024-04-14 11:23:03 +0530

    Ags: updated mpris module of bar

--- home-manager/ags/widgets/bar/modules/Mpris.ts
+++ home-manager/ags/widgets/bar/modules/Mpris.ts
@@ -1,13 +1,10 @@
 import Mpris from "resource:///com/github/Aylur/ags/service/mpris.js";
-import { Capatilize } from "utils";
-import { floatingMediaPlayer } from "utils.ts";
+import { MprisPlayer } from "types/service/mpris";
 
 function statusIcon(status: "Playing" | "Paused") {
   if (status === "Playing") return "󰏤";
   else return "";
 }
-
-const ignorePlayers = ["firefox", "chromium"];
 
 export const playerIcons = (name: string) => {
   switch (name) {
@@ -24,30 +21,50 @@ export const playerIcons = (name: string) => {
   }
 };
 
+const Player = (player: MprisPlayer) => {
+  const mpris = player.getPlayer("spotify") || player.getPlayer();
+  const { name, track_artists, track_title, play_back_status } = mpris;
+
+  const icon = Widget.Label({
+    class_name: "player-icon",
+    label: playerIcons(name),
+  });
+
+  const title = Widget.Label({
+    class_name: "title",
+    max_width_chars: 24,
+    truncate: "end",
+    wrap: true,
+    label: track_title,
+  });
+
+  const artist = Widget.Label({
+    class_name: "artist",
+    max_width_chars: 24,
+    truncate: "end",
+    wrap: true,
+    label: track_artists.join(", ") + " -",
+  });
+
+  const status = Widget.Label({
+    class_name: "status-icon",
+    label: statusIcon(play_back_status),
+  });
+
+  return Widget.Box({
+    spacing: 6,
+    children: [icon, artist, title, status],
+  });
+};
+
 export default () =>
   Widget.Button({
     class_name: "media",
-    on_primary_click: () =>
-      (floatingMediaPlayer.value = floatingMediaPlayer.value ? false : true),
+    on_primary_click: () => Mpris.getPlayer("")?.playPause(),
     on_scroll_up: () => Mpris.getPlayer("")?.next(),
     on_scroll_down: () => Mpris.getPlayer("")?.previous(),
-    child: Widget.Label("-").hook(Mpris, (self) => {
-      const player = Mpris.getPlayer("spotify") || Mpris.getPlayer();
-
-      if (player) {
-        const { name, track_artists, track_title, play_back_status } = player;
-
-        if (!ignorePlayers.includes(name)) {
-          self.label =
-            playerIcons(name) +
-            " " +
-            track_artists.join(", ") +
-            " - " +
-            track_title +
-            " " +
-            statusIcon(play_back_status);
-        } else
-          self.label = `${playerIcons(name)} ${Capatilize(name)} is playing something ${statusIcon(play_back_status)}`;
-      } else self.label = "";
-    }),
+    setup: (self) =>
+      self.hook(Mpris, (self) => {
+        self.child = Player(Mpris);
+      }),
   });
