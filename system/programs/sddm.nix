e27f8646cbaef4eddc4296313387ae51997dfbfe
RAY <souradeep_roy@pm.me>
2024-05-22 09:50:52 +0530

    Updated sddm.nix

--- system/programs/sddm.nix
+++ system/programs/sddm.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 {
-    services.xserver = {
-        displayManager.sddm = {
+    services.displayManager = {
+        sddm = {
             enable = true;
             wayland.enable = true;
             theme = "${import ../themes/sddm-theme.nix { inherit pkgs; }}";
