e82abca0d95bb00f4d5a4c7cd1b262ea9a787028
ImRayy <souradeep_roy@pm.me>
2024-06-19 11:50:01 +0530

    Fixed nvidia driver issues with latest hyprland

--- system/drivers/nvidia.nix
+++ system/drivers/nvidia.nix
@@ -4,10 +4,14 @@
     hardware.opengl.enable = true;
       services.xserver.videoDrivers = ["nvidia"];
       boot.kernelPackages = pkgs.linuxPackages_latest;
+      boot.kernelParams = [
+          "nvidia-drm.modeset=1"
+          "nvidia-drm.fbdev=1"
+      ];
       hardware.nvidia = {
           modesetting.enable = true;
           open = false;
           nvidiaSettings = false;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+          package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
 }
