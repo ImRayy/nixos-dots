a9112c20a479282138b9295124a6d403cb0b4f93
ImRayy <souradeep_roy@pm.me>
2024-11-09 01:44:12 +0530

    Updated system/programs.nix

--- system/programs.nix
+++ system/programs.nix
@@ -36,11 +36,21 @@
   };
 
   fonts = {
+    fontDir.enable = true;
     fontconfig.enable = true;
     packages = with pkgs; [
       cantarell-fonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-extra
+      dejavu_fonts
+      liberation_ttf
       (nerdfonts.override { fonts = [ "CommitMono" ]; })
     ];
   };
 
+  services.xserver.excludePackages = [ pkgs.xterm ];
 }
