b39bf8bd8bd7eae0aa1d00709315afc91c0068fa
ImRayy <souradeep_roy@pm.me>
2024-12-30 10:00:16 +0530

    Updated firewall.nix

--- system/firewall.nix
+++ system/firewall.nix
@@ -1,13 +1,20 @@
 {
-  networking.firewall.allowedTCPPorts = [
-    8384
-    22000
-    1714
-  ];
-  networking.firewall.allowedUDPPorts = [
-    22000
-    21027
-    1764
-  ];
-  networking.firewall.enable = true;
+  networking.firewall = rec {
+    enable = true;
+    allowedTCPPorts = [
+      8384
+      22000
+    ];
+    allowedUDPPorts = [
+      22000
+      21027
+    ];
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
 }
