1150293d03bfdd5c7b7fe58d663d927cc236416c
ImRayy <souradeep_roy@pm.me>
2025-06-28 19:52:53 +0530

    Updated session variables

--- home-manager/env.nix
+++ home-manager/env.nix
@@ -1,9 +1,16 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.sessionVariables = {
     NIXPKGS_ALLOW_UNFREE = "1";
     NIXOS_OZONE_WL = "1";
     GOPATH = "${config.home.homeDirectory}/.local/share/go";
     GOMODCACHE = "${config.home.homeDirectory}/.cache/go/pkg/mod";
     VOLTA_HOME = "${config.home.homeDirectory}/.volta";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+    SHELL = "${pkgs.fish}/bin/fish";
   };
 }
