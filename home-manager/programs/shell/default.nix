d0dec86180c74dc82df42bd8eb629cbb5d084f1c
ImRayy <souradeep_roy@pm.me>
2025-06-28 19:54:42 +0530

    Fzf: fixed alt + c command broken tree view

--- home-manager/programs/shell/default.nix
+++ home-manager/programs/shell/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   imports = [
     ./fish.nix
     ./starship.nix
@@ -6,17 +6,20 @@
 
   programs.fzf = {
     enable = true;
+    enableFishIntegration = true;
+    enableBashIntegration = true;
     defaultOptions = [
       "--height 40%"
       "--border"
     ];
     defaultCommand = "fd --type f --strip-cwd-prefix";
+    changeDirWidgetCommand = "fd --type directory --hidden";
     fileWidgetOptions = [
       "--preview 'bat --style=numbers --color=always --line-range :42 {}'"
     ];
-    changeDirWidgetOptions = ["--preview 'tree -C {} | head -200'"];
-    enableFishIntegration = true;
-    enableBashIntegration = true;
+    changeDirWidgetOptions = [
+      "--preview '${pkgs.eza}/bin/eza --tree {} | head -200'"
+    ];
   };
 
   programs.zoxide = {
