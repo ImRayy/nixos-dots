822fb63db4e0989a63d33c9280da97e3338d9be2
ImRayy <souradeep_roy@pm.me>
2025-11-07 23:10:58 +0530

    Fixed nvidia error "libEGL failed to create dri2 screen" while launching certain apps

--- system/drivers/nvidia.nix
+++ system/drivers/nvidia.nix
@@ -1,7 +1,12 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   nixpkgs.config.cudaSupport = true;
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
+  environment.sessionVariables."__EGL_VENDOR_LIBRARY_FILENAMES" = "${config.hardware.nvidia.package}/share/glvnd/egl_vendor.d/10_nvidia.json";
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
